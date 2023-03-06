5a593b8ee76ed362c2db7f1e1ac18c1c55d6598c
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-03-06 21:36:11 +0000

    Added readme file

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # team-profile-generator
+
+## Description
+
+team-profile-generator is a Node.js command-line application which takes in information about employees on a software engineering team using the [Inquirer package](https://www.npmjs.com/package/inquirer), then generates an HTML webpage that displays summaries for each person. 
+
+## User Story
+
+As a manager a user want to generate a webpage that displays my team's basic info so that a user have quick access to their emails and GitHub profiles.
+
+## Instructions
+
+```bash
+node index.js
+```
+
+<p float="center">
+<img src="./assets/screenshot1.png"  width="300"/>
+</p>
+
+<a href="" > Link to the demo </a>
--- src/page-template.js
+++ src/page-template.js
@@ -3,8 +3,8 @@ const generateTeam = (team) => {
   // creates the manager html
   const generateManager = (manager) => {
     return `
-        <div class="card employee-card">
-        <div class="card-header manager">
+        <div class="card employee-card m-2">
+        <div class="card-header bg-primary text-white h-100">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -22,8 +22,8 @@ const generateTeam = (team) => {
   // creates the html for engineers
   const generateEngineer = (engineer) => {
     return `
-        <div class="card employee-card">
-    <div class="card-header engineer">
+        <div class="card employee-card m-2">
+    <div class="card-header bg-success text-white h-100">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -41,8 +41,8 @@ const generateTeam = (team) => {
   // creates the html for interns
   const generateIntern = (intern) => {
     return `
-        <div class="card employee-card">
-    <div class="card-header intern">
+        <div class="card employee-card m-2">
+    <div class="card-header bg-warning text-white h-100">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
@@ -100,12 +100,12 @@ module.exports = (team) => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
+            <div class="col-12 jumbotron mb-3 bg-danger text-white">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="team-area col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
