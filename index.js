dbba3e9a05f01f701816a13d76a6dc46a4197c27
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-03-06 15:29:31 +0000

    Displayed rendered html in console

--- index.js
+++ index.js
@@ -1,251 +1,246 @@
-// import Manager from "./lib/Manager";
-// import Engineer from "./lib/Engineer";
-// import Intern from "./lib/Intern";
-import inquirer from "inquirer";
-// import { resolve, join } from "path";
-// import fs from "fs";
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
-// const OUTPUT_DIR = resolve(__dirname, "output");
-// const outputPath = join(OUTPUT_DIR, "team.html");
+const inquirer = require("inquirer");
 
-// import render from "./src/page-template.js";
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const renderHTML = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const nextPrompt = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "list",
-      message: "What do you want to do next?",
-      name: "next",
-      choices: ["Add an engineer", "Add an intern", "Finish building team"],
-    },
-  ]);
-  return response;
-};
+const nextPrompt = [
+  {
+    type: "list",
+    message: "What do you want to do next?",
+    name: "next",
+    choices: ["Add an engineer", "Add an intern", "Finish building team"],
+  },
+];
 
+const engineerPromptQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your name";
+      } else {
+        return true;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your employee ID";
+      } else {
+        return true;
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your email";
+      } else {
+        return true;
+      }
+    },
+  },
+  {
+    type: "input",
+    message: "What is your GitHub name?",
+    name: "github",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your GitHub name";
+      } else {
+        return true;
+      }
+    },
+  },
+];
 
-const engineerPrompt = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-      validate: (input_1_1) => {
-        if (!input_1_1) {
-          return "Enter your name";
-        } else {
-          return true;
-        }
-      },
+const internPromptQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your name";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your employee ID?",
-      validate: (input_3_1) => {
-        if (!input_3_1) {
-          return "Enter your employee ID";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your employee ID";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-      validate: (input_5_1) => {
-        if (!input_5_1) {
-          return "Enter your email";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your email";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      message: "What is your GitHub name?",
-      name: "github",
-      validate: (input_7_1) => {
-        if (!input_7_1) {
-          return "Enter your GitHub name";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    message: "What school did you graduate from?",
+    name: "school",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your school";
+      } else {
+        return true;
+      }
     },
-    // {
-    //   type: "list",
-    //   message: "What do you want to do next?",
-    //   name: "next",
-    //   choices: ["Add an engineer", "Add an intern", "Finish building team"],
-    // },
-  ]);
-  return response;
-};
+  },
+];
 
-//console.log(await engineerPrompt());
-
-const internPrompt = async () => {
-  const response = await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-      validate: (input_1_1) => {
-        if (!input_1_1) {
-          return "Enter your name";
-        } else {
-          return true;
-        }
-      },
+const managerPromptQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+    validate: (input_1) => {
+      if (!input_1) {
+        return "Enter your name";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your employee ID?",
-      validate: (input_3_1) => {
-        if (!input_3_1) {
-          return "Enter your employee ID";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your employee ID?",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your employee ID";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-      validate: (input_5_1) => {
-        if (!input_5_1) {
-          return "Enter your email";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your email";
+      } else {
+        return true;
+      }
     },
-    {
-      type: "input",
-      message: "What school did you graduate from?",
-      name: "school",
-      validate: (input_7_1) => {
-        if (!input_7_1) {
-          return "Enter your school";
-        } else {
-          return true;
-        }
-      },
+  },
+  {
+    type: "input",
+    message: "What is your office number?",
+    name: "officeNumber",
+    validate: (input) => {
+      if (!input) {
+        return "Enter your office number";
+      } else {
+        return true;
+      }
     },
-    // {
-    //   type: "list",
-    //   message: "What do you want to do next?",
-    //   name: "next",
-    //   choices: ["Add an engineer", "Add an intern", "Finish building team"],
-    // },
-  ]);
-  return response;
-};
-
-//console.log(await internPrompt());
-
-const managerPrompt = async () => {
-  const response = await inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your name?",
-        validate: (input_1) => {
-          if (!input_1) {
-            return "Enter your name";
-          } else {
-            return true;
-          }
-        },
-      },
-      {
-        type: "input",
-        name: "id",
-        message: "What is your employee ID?",
-        validate: (input_3) => {
-          if (!input_3) {
-            return "Enter your employee ID";
-          } else {
-            return true;
-          }
-        },
-      },
-      {
-        type: "input",
-        message: "What is your email?",
-        name: "email",
-        validate: (input_5) => {
-          if (!input_5) {
-            return "Enter your email";
-          } else {
-            return true;
-          }
-        },
-      },
-      {
-        type: "input",
-        message: "What is your office number?",
-        name: "officeNumber",
-        validate: (input_7) => {
-          if (!input_7) {
-            return "Enter your office number";
-          } else {
-            return true;
-          }
-        },
-      },
-      // {
-      //   type: "list",
-      //   message: "What do you want to do next?",
-      //   name: "next",
-      //   choices: ["Add an engineer", "Add an intern", "Finish building team"],
-      // },
-    ]);
-    return response;
-}
-
-//console.log(await managerPrompt());
+  },
+];
 
 let employees = [];
-const promptUser = async () => {
-  await managerPrompt()
-    .then(async (manager) => {
-      employees.push(manager);
-      let response = await nextPrompt();
-      while (response.next !== "Finish building team") {
-        if (response.next === "Add an engineer") {
-          console.log("***************************************");
-          console.log("Adding an engineer");
-          let engineer = await engineerPrompt();
-          employees.push(engineer);
-          response = await nextPrompt();
+
+let promptUser = () => {
+  // Manager questions
+  inquirer.prompt(managerPromptQuestions).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.officeNumber
+    );
+    employees.push(manager);
+
+    // Buiding a team: all employees except manager
+    function teamMembers() {
+      inquirer.prompt(nextPrompt).then(async (answer) => {
+        
+        while (answer.next !== "Finish building team") {
+          
+          // Add an engineer
+          if (answer.next === "Add an engineer") {
+            console.log("***************************************");
+            console.log("Adding an engineer");
+
+            // Engineer questions
+            await inquirer.prompt(engineerPromptQuestions).then((response) => {
+              const engineer = new Engineer(
+                response.name,
+                response.id,
+                response.email,
+                response.github
+              );
+              employees.push(engineer);
+            });
+          }
+
+          // Add an intern
+          if (answer.next === "Add an intern") {
+            console.log("***************************************");
+            console.log("Adding an intern");
+            // Intern questions
+            await inquirer.prompt(internPromptQuestions).then((response) => {
+              const intern = new Intern(
+                response.name,
+                response.id,
+                response.email,
+                response.school
+              );
+              employees.push(intern);
+            });
+          }
+
+        await inquirer.prompt(nextPrompt)
+        .then((response) => answer = response) ;
         }
-        if (response.next === "Add an intern") {
-          console.log("***************************************");
-          console.log("Adding an intern");
-          let intern = await internPrompt();
-          employees.push(intern);
-          response = await nextPrompt();
-        }
-      }
-    });
-    console.log(employees);
-    return employees;
+
+
+        // Render HTML file
+        renderHTML(employees);
+        console.log(renderHTML(employees));
+      });
+    }
+
+    teamMembers();
+  });
 };
 
-// Bonus using async/await and try/catch
-const init = async () => {
-  try {
-    promptUser();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-init();
+promptUser();
